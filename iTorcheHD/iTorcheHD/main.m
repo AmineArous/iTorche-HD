ee8ddc292d8b5e723d804ac191a7460b5e5354c1
TamJack <aminearous@live.fr>
2012-09-13 11:52:10 +0200

    v1

---
+++ iTorcheHD/iTorcheHD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iTorcheHD
+//
+//  Created by Amine Arous on 13/09/12.
+//  Copyright (c) 2012 Amine Arous. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
